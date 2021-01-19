ac5db9f715b29454f155da6332aba8893b53d9be
Rik <mr.rv.asd@gmail.com>
2021-01-19 15:31:00 +0100

    [Add] layout for directions

---
+++ app/src/main/java/com/study/locationawareapp/ui/directions/DirectionAdapter.java
@@ -0,0 +1,35 @@
+package com.study.locationawareapp.ui.directions;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.study.locationawareapp.ui.destination.DestinationAdapter;
+
+public class DirectionAdapter extends RecyclerView.Adapter<DirectionAdapter.ViewHolder>{
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
